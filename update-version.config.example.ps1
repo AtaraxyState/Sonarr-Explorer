a8dbbef5f23c0af177177a9ab6eb2ea8b5ea301a
Icathian Rain <Timothee.lamazieres@gmail.com>
2025-06-06 03:05:55 +0200

    🙈Update ignore to ignore version upgrade script config

---
+++ update-version.config.example.ps1
@@ -0,0 +1,11 @@
+# Configuration file for update-version.ps1
+# Copy this file to "update-version.config.ps1" and customize the path below
+
+# Set your default copy path here
+# This will be used when no -CopyToPath parameter is provided
+$DefaultCopyPath = "C:\Your\Custom\Path\Here"
+
+# Examples:
+# $DefaultCopyPath = "C:\Users\YourName\Documents\Flow.Launcher.Plugins\plugins"
+# $DefaultCopyPath = "D:\Git\Flow.Launcher.Plugins\plugins"
+# $DefaultCopyPath = "$env:USERPROFILE\Desktop\PluginSubmissions" 
